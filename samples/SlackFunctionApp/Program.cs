38f949bbe1cd76a8b5ed741c21933b40bf460944
Dominique St-Amand <dominique@webmotions.ca>
2023-11-29 21:09:30 -0500

    WIP

---
+++ samples/SlackFunctionApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SlackFunctionApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            #if DEBUG
+                Console.WriteLine($"Azure Functions .NET Worker (PID: { Environment.ProcessId }) initialized in debug mode.");
+            #endif
+
+            var host = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults()
+                .ConfigureServices(services =>
+                {
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
